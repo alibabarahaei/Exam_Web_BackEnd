78dcffcce0e2f90f82142b735b3ce1da475d12e0
AliBabarahaei <alibabarahaei@gmail.com>
2022-06-30 21:59:54 +0430

    Add Register and Login Page

---
+++ Exam_Web/Pages/Register/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Exam_Web.Pages.Register
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
